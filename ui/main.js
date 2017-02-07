fb5ed596a6a620b0badc2d23d6aef343539983d8
skkumarsparsh <skkumarsparsh@gmail.com>
2017-02-07 19:57:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,10 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + "px";
 }
 
 madi.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
