7555c4546fda937aa3f78bfc75b3412fda0dcd15
skkumarsparsh <skkumarsparsh@gmail.com>
2017-02-07 20:24:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,7 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New Value'
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
-}
-
-madi.onclick = function() {
-    var interval = setInterval(moveRight, 10);
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
