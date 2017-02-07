96667032bfd5747fa69ce5007565cc45122dde12
skkumarsparsh <skkumarsparsh@gmail.com>
2017-02-07 19:48:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value'
+
+var img = document.getElementById('madi');
+madi.onclick = function() {
+    madi.style.marginLeft = '100px';
+};
