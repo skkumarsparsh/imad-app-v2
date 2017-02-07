cca7e3f30c703b34a456f3e74b68edd70742e823
skkumarsparsh <skkumarsparsh@gmail.com>
2017-02-07 20:39:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,15 @@
-var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === HttpRequest = "DONE") {
+            if(request.status===200){
+               var counter =  request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+            }
+        }
+    };
+    resource.open('GET','http://skkumarsparsh.imad.hasura.io/counter', true);
+    resource.send(null);
 };
