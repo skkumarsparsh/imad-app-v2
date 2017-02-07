14319ccbb5dafb11bf5d22dc026bb3a6b9c87880
skkumarsparsh <skkumarsparsh@gmail.com>
2017-02-07 22:00:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,6 @@ button.onclick = function() {
 };
 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
@@ -35,6 +33,8 @@ submit.onclick = function() {
             }
         }
     };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://skkumarsparsh.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
