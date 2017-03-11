ffa7de7bb4ed944689bb9453f2e77557d65dbc56
skkumarsparsh <skkumarsparsh@gmail.com>
2017-03-11 21:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'skkumarsparsh',
+    database: 'skkumarsparsh',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,8 +82,23 @@ function createTemplate(data) {
     `;
     return HtmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+
+app.get('/', function(req, res) {
+    // make a select request
+    // return a response with the results
+    Pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
