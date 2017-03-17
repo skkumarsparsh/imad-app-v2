92c36b2e9d11e7d9d26257a95a87f6864343665f
skkumarsparsh <skkumarsparsh@gmail.com>
2017-03-17 22:07:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
 function hash(input, salt) {
     //How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbdkf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req, res) {
