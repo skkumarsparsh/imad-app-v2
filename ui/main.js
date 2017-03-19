f9c00a861ee37372c626dc6a3d80693d62b2aa21
skkumarsparsh <skkumarsparsh@gmail.com>
2017-03-19 14:07:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,4 @@
-var button = document.getElementById('counter');
-button.onclick = function() {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status===200){
-               var counter =  request.responseText;
-               var span = document.getElementById('count');
-               span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://skkumarsparsh.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+
 
 
 var submit = document.getElementById('submit_btn');
@@ -21,20 +7,20 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status===200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0;i<names.length;i++)
-                {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+                console.log('user logged in');
+                alert('Logged in successfully');
+            } else if(request.status===403) {
+                alert("Username/password is invalid");
+            } else if(request.status===500) {
+                alert("Something went wrong on the server");
             }
         }
     };
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://skkumarsparsh.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://skkumarsparsh.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
